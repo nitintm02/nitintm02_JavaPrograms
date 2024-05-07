d31a7d5af3f633c6f9241dc7a765c7121d995e89
nitintm02 <nitin.tm02@gmail.com>
2024-05-07 21:42:45 +0530

    second commit

---
+++ WhileLoopEx1.java
@@ -0,0 +1,15 @@
+package Package1;
+
+public class WhileLoopEx1 {
+
+	public static void main(String[] args) {
+		 int age=0;
+		 while (age<=5)
+		 {
+			 System.out.println(age);
+			 age++;
+			 
+		 }
+	}
+
+}
